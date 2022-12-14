1bb8c77a98f52ad39deecc9e72d254d795ac34d8
MarsX-2002 <82014315+MarsX-2002@users.noreply.github.com>
2022-12-14 17:33:35 +0500

    Add files via upload

---
+++ voice_assistant/voice_assistant_asuna.py
@@ -0,0 +1,22 @@
+import pyttsx3 as tts
+import speech_recognition
+import sys
+from neuralintents import GenericAssistant
+
+
+
+# engine = p.init()
+# rate = engine.getProperty('rate')  # speed of the voice
+# engine.setProperty('rate', 180)
+# voices = engine.getProperty('voices')
+# engine.setProperty('voices',voices[0].id)
+# print(voices)
+# engine.say("Nice to meet you. Let's be friends")
+
+
+# engine.runAndWait()
+
+
+
+
+
